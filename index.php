d52ce3f1108632fc43be13f569fd934b69b33371
ALEJANDRA BAUTISTA CELESTINO <alejandra.bautista@ddvcmx.onmicrosoft.com>
2024-09-02 13:47:28 -0600

    se agregaron los colores a la hoja de estilos y comenzando estilos de login

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Users - ¡STRATEGY</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  
+  <?php include_once "./login.php"; ?>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<div class="container-xl h-100 d-flex flex-column justify-content-center align-items-center">
+  <div class="d-flex flex-column justify-content-center align-items-center">
+    <img src="./assets/LOGO-iSTRATEGY-.png" alt="logo de iSTRATEGY" id="logo-login">
+    <p class="font-regular fs-1 text-center cl-gray-blue">Iniciar Sesión</p>
+    <p class="font-regular fs-3 text-center cl-black">Ingresa tus datos a continuación</p>
+  </div>
+
+  <div>
+    <form action="" method="POST" class="">
+
+    </form>
+  </div>
+</div>
