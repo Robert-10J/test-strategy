b274962b01c3ee34331690403c205140e3c7092e
ALEJANDRA BAUTISTA CELESTINO <alejandra.bautista@ddvcmx.onmicrosoft.com>
2024-09-02 22:38:17 -0600

    diseño de listado de usuarios empezado

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,59 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-  <?php include_once "./agregar_usuario.php"; ?>
+  <!-- <?php /* include_once "./agregar_usuario.php";  */ ?> -->
+
+  <div class="vh-100 bg-gray-blue">
+    <header class="row" style="height: 6rem; width: 100%;">
+      header
+    </header>
+
+    <main class="row h-100 bg-white">
+      <div class="col-lg-5 bg-gray-blue" style="width: 15rem;">
+        roles
+      </div>
+
+      <div class="col w-100 p-0">
+        <table class="table">
+          <thead>
+            <tr class="cl-red-btn text-white">
+              <th scope="col">No</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Email</th>
+              <th scope="col">Teléfono</th>
+              <th scope="col">Rol</th>
+              <th scope="col">Fecha de unión</th>
+              <th scope="col">Salario</th>
+              <th scope="col">Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  </div>
+
+
 </body>
+
 </html>
